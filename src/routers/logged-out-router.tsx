b9568f58ce493172dd002f8091611c67d8b01ce1
kalzpi <kalzpi@gmail.com>
2021-03-25 06:17:37 +0900

    15.2 React Hook Form MAGIC

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,3 +1,54 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { isLoggedInVar } from '../apollo';
 
-export const LoggedOutRouter = () => <span>Logged Out</span>;
+interface IForm {
+  email: string;
+  password: string;
+}
+
+export const LoggedOutRouter = () => {
+  const { register, watch, handleSubmit, errors } = useForm<IForm>();
+  const onSubmit = () => {
+    console.log(watch('email'));
+  };
+  const onInvalid = () => {
+    console.log(errors);
+    console.log('Cannot do it');
+  };
+  return (
+    <div>
+      <h1>Logged Out</h1>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          {errors.email?.message && (
+            <span className='text-red-400'>{errors.email.message}</span>
+          )}
+          {errors.email?.type === 'validate' && (
+            <span className='text-red-400'>Only Gmail allowed</span>
+          )}
+          <input
+            ref={register({
+              required: 'This field is required.',
+              validate: (email: string) => email.includes('@gmail.com'),
+            })}
+            type='email'
+            name='email'
+            placeholder='email'
+          />
+        </div>
+        <div>
+          <input
+            ref={register({
+              required: true,
+            })}
+            type='password'
+            name='password'
+            placeholder='password'
+          />
+        </div>
+        <button className='bg-yellow-300 text-white'>Sumit</button>
+      </form>
+    </div>
+  );
+};
